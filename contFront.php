686edd66a242e634f103e793bf0ddd103b9a8617
Altior1 <thomas.rud@outlook.fr>
2024-09-10 19:33:33 +0200

    début du projet

---
+++ contFront.php
@@ -0,0 +1,21 @@
+<?php
+ include "controlleur/controllePage.php"
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Restaurant Japonais</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="vue\CSS\style.css">
+    </head>
+    <body>
+        <?php include "vue/caneva/header.html";
+        ?>
+        <?php include "vue\accueil.html";
+        ?>
+        <?php include "vue/caneva/footer.html"; 
+        ?>
+    </body>
+</html>
