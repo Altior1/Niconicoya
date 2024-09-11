3da89df6f8400098b2127d7c08f28a799d665f79
Altior1 <thomas.rud@outlook.fr>
2024-09-11 14:29:10 +0200

    lien entre les différentes pages opérationnel

--- contFront.php
+++ contFront.php
@@ -1,5 +1,33 @@
 <?php
- include "controlleur/controllePage.php"
+ include "controlleur/controllePage.php";
+if(isset($_GET['pages'])){
+    switch($_GET['pages']){
+        case "accueil":
+            $page="vue\accueil.html";
+            break;
+        case "plats":
+            $page= "vue\plats.html";
+            break;
+        case "boisson":
+            $page= "vue\boissons.html";
+            break;
+        case "mention":
+            $page= "vue\mentions.html";
+            break;
+        case "contact":
+            $page= "vue\contact.html";
+            break;
+        case "carte":
+            $page= "vue\carte.html";
+            break;
+        case "groupe":
+            $page= "vue\groupes.html";
+            break;
+    }
+}
+else{
+    $page= "vue\accueil.html";
+}
 
 ?>
 
@@ -13,8 +41,9 @@
     <body>
         <?php include "vue/caneva/header.html";
         ?>
-        <?php include "vue\accueil.html";
-        ?>
+        <div class="principal">
+        <?php include $page;
+        ?> </div>
         <?php include "vue/caneva/footer.html"; 
         ?>
     </body>
